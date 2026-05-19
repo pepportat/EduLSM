966269df76e9f7872a7a351b8989b9ec469b6e89
Giorgi Jorjikia <jorjikiaguga@gmail.com>
2026-05-19 22:04:53 +0400

    feat: add bogus and use faker to generate random word for insert value

--- src/Main/UIHandlers/LsmEngine.cs
+++ src/Main/UIHandlers/LsmEngine.cs
@@ -1,10 +1,13 @@
+using Bogus;
 using Core.Common;
 using Core.MemTables;
 using Core.MemTables.RedBlackTree;
 using Core.MemTables.RedBlackTree.VisualizerHelpers;
+using Core.SSTables;
 using Main.Helpers;
 using Raylib_cs;
 using static Core.SSTables.VisualizerHelpers.ReadAllSsTables;
+using static Core.SSTables.Search;
 
 namespace Main.UIHandlers;
 
@@ -14,7 +17,9 @@ public partial class LsmEngine
     private IMemTable Tree { get; set;}
     private Dictionary<int, NodeSnapshot> Layout { get; set; }
     private List<MemTableStep> Steps { get; set; }
+    private List<SearchResult> SsTablesSearchResults { get; set; }
     public Font Font { get; set; }
+    private Faker Faker { get; set; }
     
     private readonly string _dataPath;
     
@@ -24,15 +29,26 @@ public partial class LsmEngine
         Tree = new RedBlackTree();
         Layout = Tree.GetLayout();
         Steps = [];
+        SsTablesSearchResults = [];
         _maxMemTableCount = programOptions.MaxMemTableCount;
         _dataPath = Path.Combine(programOptions.DataPath, FileConstants.DataDirectoryName);
-
+        Faker = new Faker();
+        
+        
         if (!Directory.Exists(_dataPath))
         {
             Directory.CreateDirectory(_dataPath);
         }
         
         SsTables = ReadAllTables(_dataPath);
+    }
+
+    private void Search()
+    {
+        var key = int.Parse(UiState.Input);
+        var (_, list) = Tree.Get(key);
+        Steps = list;
+        SsTablesSearchResults = SearchKey(key, _dataPath).ToList();
     }
 
     private bool TryGetCurrentStep(out MemTableStep step)
