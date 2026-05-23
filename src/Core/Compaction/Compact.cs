d7084b5f9bbefa2b0cf4a203add100e7f9e37e9c
pepportat <lazarebedukadze@gmail.com>
2026-05-23 19:53:26 +0400

    Added class to iterate over sstable data entries

---
+++ src/Core/Compaction/Compact.cs
@@ -0,0 +1,76 @@
+﻿using Core.SSTables.IoHelpers;
+using Core.SSTables.IoOperations;
+using Core.SSTables.Structure;
+
+namespace Core.Compaction;
+
+public class Compact
+{
+    public void CompactFiles(string directoryPath)
+    {
+        // 1. Read all files
+        var ssTables = LoadSsTables(directoryPath);
+        var chunks = ssTables.ToLookup(f =>
+        {
+            if (f.FileName.StartsWith("t1"))
+            {
+                return 1;
+            }
+
+            if (f.FileName.StartsWith("t2"))
+            {
+                return 2;
+            }
+
+            return 3;
+        });
+
+        var l = chunks[1];
+
+        // 2. Compact them
+
+
+        // 3. Write to disk
+
+        // 4. Delete old files 
+    }
+
+    private List<SsTable> LoadSsTables(string directoryPath)
+    {
+        var filePaths = Directory.GetFiles(directoryPath).Reverse().ToArray();
+        var results = new List<SsTable>();
+        foreach (var filePath in filePaths)
+        {
+            var result = new SsTable();
+
+            result.FileName = Path.GetFileName(filePath);
+
+            using var stream = File.OpenRead(filePath);
+            using var reader = new BinaryReader(stream);
+
+            var footer = FileReader.ReadFooter(reader);
+            result.Footer = footer;
+
+            reader.BaseStream.Position = footer.DataBlockOffset;
+            var fileInfo = new FileInfo(filePath);
+            var data = FileReader.ReadDataBlock(reader, fileInfo.Length);
+
+            results.Add(result);
+        }
+
+        return results;
+    }
+
+    /*
+    private List<SsTable> IterateAndCompact(List<string> fileNames)
+    {
+        var filesSorted = fileNames.OrderBy(f => f).ToList();
+        var fileIterator1 = new DataBlockIterator(filesSorted[1]);
+        var fileIterator2 = new DataBlockIterator(filesSorted[2]);
+        var fileIterator3 = new DataBlockIterator(filesSorted[3]);
+        
+        var next1
+    }
+    */
+    
+}
---
+++ src/Core/SSTables/IoHelpers/DataBlockIterator.cs
@@ -0,0 +1,60 @@
+﻿using Core.Common;
+using Core.SSTables.IoOperations;
+using Core.SSTables.Structure;
+
+namespace Core.SSTables.IoHelpers;
+
+public class DataBlockIterator : IDisposable
+{
+    private string FilePath { get; set; }
+
+    private long IteratingOffset { get; set; }
+    private long EndOffset { get; set; }
+    private FileStream FileStream { get; set; }
+    private BinaryReader Reader { get; set; }
+
+    /// <summary>
+    /// Initializes the DataBlockIterator class.
+    /// </summary>
+    /// <param name="filePath">Path for Sstable file</param>
+    public DataBlockIterator(string filePath)
+    {
+        FilePath = filePath;
+        FileStream = File.OpenRead(FilePath);
+        Reader = new BinaryReader(FileStream);
+
+        Reader.BaseStream.Seek(-MetaData.ByteLenght, SeekOrigin.End);
+
+        IteratingOffset = Reader.ReadInt64();
+        EndOffset = Reader.ReadInt64();
+
+        Reader.BaseStream.Position = IteratingOffset;
+    }
+
+    /// <summary>
+    /// Iterates through the Sstable
+    /// </summary>
+    /// <param name="result"></param>
+    /// <returns></returns>
+    public bool Next(out Kvp? result)
+    {
+        if (IteratingOffset >= EndOffset)
+        {
+            result = null;
+            return false;
+        }
+
+        var key = Reader.ReadInt32();
+        var value = Reader.ReadString();
+        var isTombstone = Reader.ReadBoolean();
+
+        result = new(key, value, isTombstone);
+        return true;
+    }
+
+    public void Dispose()
+    {
+        FileStream.Dispose();
+        Reader.Dispose();
+    }
+}
