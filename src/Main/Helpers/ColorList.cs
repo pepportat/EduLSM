24362d7a6f474c33ceab7a5fdef686b83d878a38
Giorgi Jorjikia <jorjikiaguga@gmail.com>
2026-06-09 00:47:25 +0400

    feat: add class for holding compaction result

---
+++ src/Main/Helpers/ColorList.cs
@@ -0,0 +1,17 @@
+using Raylib_cs;
+
+namespace Main.Helpers;
+
+public static class ColorList
+{
+    public static readonly List<Color> Colors = [
+        Color.Blue,
+        Color.Purple,
+        Color.Magenta,
+        Color.Violet,
+        Color.Gold,
+        Color.Orange,
+        Color.Yellow,
+        Color.SkyBlue
+    ];
+}
